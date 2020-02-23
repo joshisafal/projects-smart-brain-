3d93a288831452998d6cd11988a2da86aba4654b
Safal Lal Joshi <joshisafalnp@gmail.com>
2020-02-23 19:26:14 +0545

    added navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      {/*<Logo />
+      <ImageLinkForm />
+      <FaceRecognition />*/}
     </div>
   );
 }
